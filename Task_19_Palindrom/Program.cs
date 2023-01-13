e5e3ece086491b42005940172c7f98bbeea15713
alex123 <alex123@dot.com>
2023-01-13 22:08:46 +0500

    Add method

--- Task_19_Palindrom/Program.cs
+++ Task_19_Palindrom/Program.cs
@@ -5,24 +5,36 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Clear();
-int number = 0;
-Console.Write("Введите пятизначное положительное целое число: ");
-int.TryParse(Console.ReadLine(), out number);
-while (number < 10000 | number > 99999)
-{
-    Console.WriteLine("Вы не выполнили условия ввода.");
-    Console.Write("Повторите ввод числа; ");
-    int.TryParse(Console.ReadLine(), out number);
+class InputNumber {
+    public int Nmb_5() {
+        int num;
+        Console.Write("Введите пятизначное положительное целое число: ");
+        int.TryParse(Console.ReadLine(), out num);
+        while (num < 10000 | num > 99999) {
+            Console.WriteLine("Вы не выполнили условия ввода.");
+            Console.Write("Повторите ввод числа: ");
+            int.TryParse(Console.ReadLine(), out num);
+        }
+        return num;
+    }
 }
 
-if (number/10000 == number%10 && number/1000%10 == number%100/10)
-{
-    Console.WriteLine($"Число {number} является полиндромом");
-    Console.WriteLine($"{number} -> Да");
+class CheckPalindrom {
+    static void Main(){
+        Console.Clear();
+         int number;
+       InputNumber obNum = new InputNumber();
+       number = obNum.Nmb_5();
+        if (number/10000 == number%10 && number/1000%10 == number%100/10) {
+            Console.WriteLine($"Число {number} является полиндромом");
+            Console.WriteLine($"{number} -> Да");
+        }
+        else {
+            Console.WriteLine($"Число {number} не является полиндромом");
+            Console.WriteLine($"{number} -> Нет");
+        }
+    }
 }
-else
-{
-    Console.WriteLine($"Число {number} не является полиндромом");
-    Console.WriteLine($"{number} -> Нет");
-}
+
+
+
