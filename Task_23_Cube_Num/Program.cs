71197012815c3bd614dba1b53ccf92b256a2a0e0
alex123 <alex123@dot.com>
2023-01-14 10:38:30 +0500

    Add class & method CubeNum

--- Task_23_Cube_Num/Program.cs
+++ Task_23_Cube_Num/Program.cs
@@ -4,21 +4,40 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.Clear();
-int number = 0;
-Console.Write("Введите положительное целое число от 1 до 30: ");
-int.TryParse(Console.ReadLine(), out number);
-while (number < 1 | number > 30)
-{
-    Console.WriteLine("Вы не выполнили условия ввода.");
-    Console.Write("Повторите ввод числа: ");
-    int.TryParse(Console.ReadLine(), out number);
+class GetNum {
+    public int CheckInt() {
+        int num;
+        Console.Write("Введите число: ");
+        int.TryParse(Console.ReadLine(), out num);
+        while (num < 1) {
+            Console.Write("Число не соответствует условиям задачи, повторите ввод числа: ");
+            int.TryParse(Console.ReadLine(), out num);
+        }
+        Console.WriteLine();
+        return num;
+    }
 }
-double count = 1;
-Console.Write($"{number} -> ");
-for (count = 1; count < number; count++)
-{
-    Console.Write($"{Math.Pow(count, 3)}, ");
+
+class PrintResult {
+    public void PrntRslt(int num) {
+        int number = num;
+        double count = 1;
+        Console.Write($"{number} -> ");
+        for (count = 1; count < number; count++) {
+            Console.Write($"{Math.Pow(count, 3)}, ");
+        }
+        Console.WriteLine($"{Math.Pow(count, 3)}.");
+        Console.WriteLine(); 
+    }
 }
-Console.WriteLine($"{Math.Pow(count, 3)}.");
-Console.WriteLine();
+
+class FindCub {
+    static void Main() {
+        Console.Clear();
+        Console.WriteLine(@"Программа, принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+        Console.WriteLine();
+        GetNum gn = new GetNum();
+        PrintResult pr = new PrintResult();
+        pr.PrntRslt(gn.CheckInt());
+    }
+}
